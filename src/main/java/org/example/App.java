67dd5dc37a4432bc3131a6d76121064171a2fa41
Vova Zimin <ziminvova44@gmail.com>
2023-10-02 11:40:50 +0300

    Temp branch (task 3)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, I wanna be a programmer" );
+        System.out.println( "Create changes into branch (temp)" );
     }
 }
